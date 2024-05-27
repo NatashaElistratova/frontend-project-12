d5a87b68cbb592de59db9db4f046a0552d72d930
NatashaElistratova <natalia.yelistratova@gmail.com>
2024-05-27 17:44:57 +0300

    Добавление хуков и контекста.

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ build:
 	rm -rf frontend/build
 	npm run postinstall
 	npm run build
+
+frontend start:
+	cd frontend
+	npm run start
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import MainPage from './Components/Pages/MainPage';
 import Login from './Components/Pages/Login';
 import NotFound from './Components/Pages/404';
 import NavbarComponent from './Components/Navbar';
 
+import AuthContext from '../contexts/index.jsx';
+import useAuth from '../hooks/index.jsx';
+
+const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem('userId');
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
 function App() {
   return (
     <div className="h-100">
       <NavbarComponent></NavbarComponent>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
+      
     </div>
     
   );
