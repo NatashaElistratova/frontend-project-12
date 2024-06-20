2b0ec4e5b1a44e659692d3aedf45b8a64d5a4e59
natalia.yelistratova <natalia.yelistratova@gmail.com>
2024-06-20 22:16:09 +0300

    Fix

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+start-frontend:
+	cd frontend
+	npm run start
 build:
 	rm -rf frontend/build
 	npm run postinstall
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,14 +1,14 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 
 function NavbarComponent() {
-  return (
-    <Navbar bg="white" data-bs-theme="light" shadow="sm">
-        <Container>
-          <Navbar.Brand href="/">MY AWESOME CHAT</Navbar.Brand>
-        </Container>
-      </Navbar>
-  );
+    return (
+        <Navbar bg="white" data-bs-theme="light" shadow="sm" fixed="top">
+            <Container>
+                <Navbar.Brand href="/">MY AWESOME CHAT</Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
 }
 
 export default NavbarComponent;
